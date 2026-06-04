29cf4b0cd1b16fb256de5a50bcaa456de1ca46a6
mmogos <mmogos@pbs.com>
2026-06-04 16:01:58 +0300

    CAT-26430 Added support for schedule_expression type events

--- main.tf
+++ main.tf
@@ -2,9 +2,17 @@ resource "aws_cloudwatch_event_rule" "event_rule" {
   name        = local.name
   description = local.description
 
-  event_pattern = var.event_pattern
+  event_pattern       = var.event_pattern
+  schedule_expression = var.schedule_expression
 
   tags = local.tags
+
+  lifecycle {
+    precondition {
+      condition     = (var.event_pattern != null) != (var.schedule_expression != null)
+      error_message = "Exactly one of event_pattern or schedule_expression must be provided."
+    }
+  }
 }
 
 resource "aws_cloudwatch_event_target" "event_target" {
@@ -27,6 +35,7 @@ resource "aws_cloudwatch_event_target" "ecs_event_target" {
     task_count          = each.value.task_count
     launch_type         = each.value.launch_type
     platform_version    = each.value.platform_version
+    propagate_tags      = each.value.propagate_tags ? "TASK_DEFINITION" : null
 
     dynamic "network_configuration" {
       for_each = each.value.network_configuration != null ? [each.value.network_configuration] : []
